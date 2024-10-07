fefdeccc17aad2fe6b403abeefcc88b309f7b12c
A. Dartagnan <dartajunior3@gmail.com>
2024-10-07 19:07:47 -0300

    update: remove old project

--- src/data/collaborators.ts
+++
@@ -1,6 +0,0 @@
-export const collaborators = [
-  { name: 'Dr. XPTO', image: '/placeholder.svg?height=200&width=200' },
-  { name: 'Prof. XYZ', image: '/placeholder.svg?height=200&width=200' },
-  { name: 'Dra. ABC', image: '/placeholder.svg?height=200&width=200' },
-  { name: 'Dr. ZPS', image: '/placeholder.svg?height=200&width=200' },
-];
