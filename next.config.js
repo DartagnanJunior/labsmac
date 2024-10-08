4c6670e1cbe71e2c22e5fa35322f739d0d49dde9
A. Dartagnan <dartajunior3@gmail.com>
2024-10-08 13:46:44 -0300

    fix images

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // basePath: "labsmac",
-  output: "export",
+  // basePath: "/labsmac",
+  output: 'export',
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
